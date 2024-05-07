ab09deb0af4d681cb138d4e57b7849a21aaa47ab
David D <davidsdankwah@gmail.com>
2024-05-07 13:05:19 +0200

    Delete DC-theme directory

--- DC-theme/wp-content/themes/DCustoms/library/theme-acf.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Block usage if file is called directly.
- */
-defined('ABSPATH') || exit('Forbidden');
-
-if (class_exists('ACF')) {
-    require get_template_directory() . '/library/acf/auto-finder.php';
-}
